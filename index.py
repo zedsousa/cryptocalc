5c0d18e804e9ce2eb85964cb398e475f1a183ffb
sousa125 <39315413+zedsousa@users.noreply.github.com>
2022-07-02 15:52:03 -0300

    Corrigir divisão

--- index.py
+++ index.py
@@ -12,13 +12,14 @@ def welcome():
 @app.route('/result', methods=['POST'])
 def result():
     var_1 = request.form.get("var_1", type=int)
-    result = var_1 // 21.3
-    volume = result * 0.01
+    result = var_1 / 21.3
+    print(result)
+    volume = round(result, 2) * 0.01
     commission = volume * 100
     goal = commission * 2
     
     entry = result
-    return render_template('result.html', entry=entry, volume=round(volume, 2), commission=commission, goal=goal)
+    return render_template('result.html', entry=entry, volume=round(volume, 2), commission=int(commission), goal=int(goal))
 
 if __name__ == '__main__':
     app.run(debug=True)
