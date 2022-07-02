b8dcb6b787651a73de47cddf0853221703198de2
sousa125 <39315413+zedsousa@users.noreply.github.com>
2022-07-02 15:23:51 -0300

    Adicionando arquivos iniciais

---
+++ index.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def welcome():
+    return render_template('form.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    var_1 = request.form.get("var_1", type=int)
+    result = var_1 // 21.3
+    volume = result * 0.01
+    commission = volume * 100
+    goal = commission * 2
+    
+    entry = result
+    return render_template('result.html', entry=entry, volume=round(volume, 2), commission=commission, goal=goal)
+
+if __name__ == '__main__':
+    app.run(debug=True)
